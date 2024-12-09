38b5063fb8118d19a2efa26e37dd3b0860a7a88f
SADOVSKV <3217cody@gmail.com>
2024-12-09 18:09:44 +0100

    lerp working

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	velocity.x = lerp(velocity.x, 0.0, 0.005)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
 	print(is_on_floor())
 	move_and_slide()
