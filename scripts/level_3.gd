6407b7d0bd981557714eb83109a6e660d509e191
Nekak <3217cody@gmail.com>
2024-12-14 14:57:36 +0100

    Wet HUD added

--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -1,18 +1,22 @@
 extends Node
 
 @onready var p = $Player
+@onready var wet = $Player/Camera2D/Wet
 
 func _process(delta: float) -> void:
 	pass
 
 func _on_water_body_entered(body: Node2D) -> void:
+	print("splash")
 	p.slip = 0.1
-	p.gravity = 0.1
+	p.gravity = 0.01
 
 
 func _on_water_body_exited(body: Node2D) -> void:
 	print("wet")
+	wet.visible = true
 	p.gravity = 1
 	var timer = get_tree().create_timer(2)
 	await  timer.timeout
 	p.slip = 0
+	wet.visible = false
