db3c808bc84478ad8a18fe7908be3e071a633a54
Nekak <3217cody@gmail.com>
2024-12-14 13:32:27 +0100

    Second level is finishable

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -5,3 +5,8 @@ func _ready() -> void:
 	tween.tween_property($Start, "modulate:a", 0, 1)
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("trans"): T.next()
+
+
+func _on_end_body_entered(body: Node2D) -> void:
+	print(body.name)
+	T.next()
