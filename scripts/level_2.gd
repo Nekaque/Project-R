6e289425aa2884ac1acf01f1f9858028914e2384
Nekak <3217cody@gmail.com>
2024-12-14 11:59:31 +0100

    Added better transition to L2

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -1,5 +1,7 @@
 extends Node
 
-
+func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Start, "modulate:a", 0, 1)
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("trans"): T.next()
