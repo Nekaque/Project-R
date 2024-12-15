9880b92d092c1cebb30cbcb287e8b3ca50aabe13
Nekak <3217cody@gmail.com>
2024-12-15 18:29:38 +0100

    Added coyote

--- scripts/me.gd
+++ scripts/me.gd
@@ -7,11 +7,14 @@ var slip = false
 var gravity = 1
 @onready var sprite = $Sprite2D
 var controlable = true
+var coyote = 0.1
 
 func _physics_process(delta: float) -> void:
 	if controlable:
 		var direction := Input.get_axis("left", "right")
-		if Input.is_action_just_pressed("jump") and is_on_floor():
+		if is_on_floor():
+			coyote = 0.1
+		if Input.is_action_just_pressed("jump") and coyote >= 0:
 			velocity.y = JUMP_VELOCITY
 		if direction != 0:
 			velocity.x = direction * speed
@@ -23,6 +26,7 @@ func _physics_process(delta: float) -> void:
 			if slip: velocity.x = lerp(velocity.x, 0.0, 0.03)
 			else: velocity.x = 0
 		if not is_on_floor():
+			coyote -= delta
 			velocity += get_gravity() * delta * gravity
 			sprite.animation = "jump"
 	else:
