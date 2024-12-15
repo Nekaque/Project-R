f3e5195a2ad0d2bec6ffe8260b74cfbe3e33c5e7
Nekak <3217cody@gmail.com>
2024-12-15 16:43:59 +0100

    chest added

--- scripts/end.gd
+++ scripts/end.gd
@@ -7,3 +7,11 @@ func _input(event: InputEvent) -> void:
 		klara.controlable = !klara.controlable
 		p.controlable = !p.controlable
 		
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
+	$Chest/Locked.visible = false
+	$Chest/Opened.visible = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(klara,"modulate:a", 1, 1)
