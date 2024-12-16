dd3b08e6489fe4e08c121483a5b396397fd0e22a
Nekak <3217cody@gmail.com>
2024-12-16 23:40:32 +0100

    Minor text changes in GE and menu

--- scripts/levels/level_1.gd
+++ scripts/levels/level_1.gd
@@ -2,15 +2,10 @@ extends Node
 
 @onready var tm = $PlayArea
 @onready var p = $Player
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#T.update("res://scenes/level_1.tscn")
-	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var pos = tm.local_to_map(p.position-Vector2(640,328))
+	var pos = tm.local_to_map(p.position - Vector2(640,328))
 	var id = tm.get_cell_tile_data(pos)
 	if (id == null): p.slip = false
 	elif (id.terrain == 0): p.slip = true
